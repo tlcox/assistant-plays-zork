54944d3998145c00e5b4293c2922a9757d818eda
Travis Cox <tlcox3@uncg.edu>
2018-10-07 06:09:49 -0400

    added restart

--- node-app/app.js
+++ node-app/app.js
@@ -4,7 +4,7 @@ const spawn = require('child_process').spawn;
 const app = express();
 const port = 8000;
 const cors = require('cors');
-
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 app.use(cors());
 
 app.use(body_parser.json());
@@ -14,7 +14,14 @@ let responses = [];
 let zork;
 
 app.get('/', (req, res) => {
-	console.log('get');
+	console.log('repeat');
+	if (!zork) {
+		response = {
+			message: 'No game is in progress.'
+		};
+		res.json(response);
+		return;
+	}
 	response = {
 		message: responses[responses.length - 1]
 	};
@@ -22,29 +29,58 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
-	console.log('post');
-	let cmd = req.body.cmd + "\n";
-	zork.stdin.write(cmd);
-	setTimeout(function () {
+	console.log('command');
+	if (!zork) {
 		response = {
-			message: responses[responses.length - 1]
+			message: 'No game is in progress.'
 		};
 		res.json(response);
-	}, 10);
+		return;
+	}
+	let cmd = req.body.cmd + "\n";
+	zork.stdin.write(cmd);
+
+	await delay(20);
+	response = {
+		message: responses[responses.length - 1]
+	};
+	res.json(response);
 
 });
 
-app.get('/startGame', (req, res) => {
+app.get('/startGame', async (req, res) => {
 	console.log('new game');
+	if (zork) {
+		zork.kill();
+		await delay(20);
+	}
 	zork = spawn('./zork');
 	zork.stdout.on('data', (data) => {
 		responses.push(data.toString());
 	});
 
-	setTimeout(function () {
+	await delay(20);
+	response = {
+		message: responses[responses.length - 1]
+	};
+	res.json(response);
+
+});
+
+app.get('/end', (req, res) => {
+	console.log('end');
+	if (!zork) {
 		response = {
-			message: responses[responses.length - 1]
+			message: 'No game is in progress.'
 		};
 		res.json(response);
-	}, 10);
+		return;
+	}
+	zork.kill();
+
+	await delay(20);
+	response = {
+		message: 'Game ended, goodbye.'
+	};
+	res.json(response);
 });
